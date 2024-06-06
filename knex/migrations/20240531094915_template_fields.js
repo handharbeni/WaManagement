39cc157c89ec5fbe2bb45ca45c369a8a63eb7ea9
Muhammad Handharbeni <mhandharbeni@gmail.com>
2024-06-06 03:25:21 +0000

    add fields value for template message

--- knex/migrations/20240531094915_template_fields.js
+++ knex/migrations/20240531094915_template_fields.js
@@ -3,7 +3,15 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('template_fields', function (t) {
+        t.increments('id').unsigned().primary();
+        t.timestamps(true, true);
+        t.integer('owner_id').unsigned();
+        t.foreign("owner_id").references("owners.id");
+        t.integer('template_id').unsigned();
+        t.foreign("template_id").references("template_message.id");
+        t.text('field', 'longtext');
+    });
 };
 
 /**
@@ -11,5 +19,6 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    let dropQuery = `DROP TABLE template_fields`
+    return knex.raw(dropQuery);  
 };
